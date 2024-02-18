3fb9ec8af941216168cbd888b3fd0658faf2ad24
fatnotfat <nguyenphat2711@gmail.com>
2024-02-18 19:18:22 +0700

    add payment feature

---
+++ Pillsy/Constants/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Pillsy.Constants
+{
+    public class Constants
+    {
+        public static string ClientId = "310b83ae-b4a3-4b85-90f5-61b2c5fabef6";
+        public static string ApiKey = "35925b24-6856-4e0b-ac8c-8b92bf506be0";
+        public static string CheckSumKey = "f7c9f9418740507c2927fa867d968c685c525b0786268440f0cdaf83960ef2b7";
+
+    }
+}
--- Pillsy/Controllers/Payments/PaymentsController.cs
+++ Pillsy/Controllers/Payments/PaymentsController.cs
@@ -1,8 +1,14 @@
-﻿using Mapster;
+﻿using BusinessObject;
+using Mapster;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Net.payOS;
+using Net.payOS.Types;
 using Pillsy.DataTransferObjects.Payment.PaymentDto;
+using Pillsy.DataTransferObjects.Pill.PillCreateWithPrescriptionDto;
 using Service.Momo.Request;
+using System.Diagnostics;
 
 namespace Pillsy.Controllers.Payments
 {
@@ -10,23 +16,43 @@ namespace Pillsy.Controllers.Payments
     [ApiController]
     public class PaymentsController : ControllerBase
     {
-        //[HttpGet]
-        //[Route("momo-return")]
-        //public async Task<IActionResult> MomoReturn([FromQuery] MomoOneTimePaymentResultRequest response)
-        //{
-        //    string returnUrl = string.Empty;
-        //    var returnModel = new PaymentReturnDto();
-        //    var processResult = await response.Adapt<ProcessMomoPaymentReturn>();
+        PayOS payOS;
+        public PaymentsController()
+        {
+            payOS = new PayOS(Constants.Constants.ClientId, Constants.Constants.ApiKey, Constants.Constants.CheckSumKey);
+        }
 
-        //    if (processResult.Success)
-        //    {
-        //        returnModel = processResult.Data.Item1 as PaymentReturnDto;
-        //        returnUrl = processResult.Data.Item2 as string;
-        //    }
+        [Authorize(Roles = "Patient")]
+        [HttpPost]
+        [Route("bank-transfer")]
+        public async Task<ActionResult<bool>> BankTransfer(string name, int number, int price)
+        {
+            try
+            {
+                ItemData item = new ItemData(name, number, price);
+                List<ItemData> items = new List<ItemData>();
+                items.Add(item);
 
-        //    if (returnUrl.EndsWith("/"))
-        //        returnUrl = returnUrl.Remove(returnUrl.Length - 1, 1);
-        //    return Redirect($"{returnUrl}?{returnModel.ToQueryString()}");
-        //}
+                var description = "Thanh toan chuyen khoan";
+                Random rnd = new Random();
+                var orderId = rnd.Next(0, 100);
+                var totalPrice = price;
+
+                PaymentData paymentData = new PaymentData(orderId, totalPrice, description, items, "https://localhost:7100/", "https://localhost:7100/");
+                CreatePaymentResult createPayment = await payOS.createPaymentLink(paymentData);
+
+                var linkCheckOut = createPayment.checkoutUrl;
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = @"C:\Program Files\Google\Chrome\Application\chrome.exe",
+                    Arguments = linkCheckOut
+                });
+                return Ok(linkCheckOut);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
